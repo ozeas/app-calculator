af54f4a0505209be2fc2dd99fce3c2cf7b5b0744
Ozeas <computer.ozeas@gmail.com>
2020-08-23 13:10:50 -0300

    Add tests and build configs in ui-components  (#4)
    * fix: fix warning typescript
    
    * fix: add folder in .gitignore
    
    * fix: fix problem version jest
    
    * fix: version jest
    
    * feat: add config jest in package.json
    
    * fix: change testMatch files jest
    
    * fix: remove mock components
    
    * fix: config tsconfig
    
    * fix: add arg to jest test
    
    * fix: remove unused file test
    
    * fix: add --passWithNoTests on app package
    
    * fix: fix prettier tsconfig
    
    * fix: remove build process ci

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App = () => (
+const App: React.FC = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        href="www.react.org"
-        className="App-link"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <header className="App-header"></header>
   </div>
 );
 
