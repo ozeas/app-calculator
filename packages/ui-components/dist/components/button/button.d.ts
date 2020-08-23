b667d8ad43fbde191ee505207aaf2356f0b4cfc6
Ozeas Santos <computer.ozeas@gmail.com>
2020-08-23 11:56:01 -0300

    fix: add folder in .gitignore

--- packages/ui-components/dist/components/button/button.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-declare const Button: ({ children }: {
-    children: any;
-}) => JSX.Element;
-export default Button;
--- packages/ui-components/dist/index.es.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-var Button = function (_a) {
-    var children = _a.children;
-    return React.createElement("button", null, children);
-};
-
-export { Button };
-//# sourceMappingURL=index.es.js.map
