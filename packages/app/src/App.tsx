a14a31c11b67b64a1788356275f2e4370a1bd2a0
Ozeas <computer.ozeas@gmail.com>
2020-08-22 21:06:43 -0300

    Add config to linters and ci (#2)
    * feat: add package app structure
    
    * fix: fix prettier json files
    
    * feat: add circleci config

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        href="www.globo.com"
+        className="App-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
