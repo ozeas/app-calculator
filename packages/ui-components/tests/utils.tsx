601d592811ab629e8d10110c58631232c8407d5e
Ozeas Santos <computer.ozeas@gmail.com>
2020-08-26 02:27:49 -0300

    fix: type return utils tests

--- packages/ui-components/tests/utils.tsx
+++ packages/ui-components/tests/utils.tsx
@@ -4,5 +4,7 @@ import { render } from '@testing-library/react';
 
 import theme from '../lib/style-tokens/theme';
 
-export const renderWithTheme = (component: ReactNode): object =>
+export const renderWithTheme = (
+  component: ReactNode
+): Record<string, unknown> =>
   render(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
