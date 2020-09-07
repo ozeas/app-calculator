a7fe11c2ff6449c7ff9ad083fce583cf4cb3a5f6
Ozeas Santos <computer.ozeas@gmail.com>
2020-09-07 19:44:46 -0300

    fix: update tests

--- packages/app/src/apps/calculator/components/calculator-page/network-status.test.tsx
+++ packages/app/src/apps/calculator/components/calculator-page/network-status.test.tsx
@@ -4,14 +4,19 @@ import { render } from '@testing-library/react';
 import NetworkStatus from './network-status';
 
 describe('NetworkStatus', () => {
+  const $offline = /Você está offline/i;
+  const $slow = /Sua conexão parece está lenta/i;
+  const $timeout = /A API não conseguiu responder a tempo/i;
+  const $error = /Houve um erro na simulação/i;
+
   describe('initial state', () => {
     it('should show empty state', () => {
       const { queryByText } = render(<NetworkStatus />);
 
-      expect(queryByText(/Você está offline/i)).toBeNull();
-      expect(queryByText(/Sua conexão parece está lenta/i)).toBeNull();
-      expect(queryByText(/A API não conseguiu responder a tempo/i)).toBeNull();
-      expect(queryByText(/Houve um erro na simulação/i)).toBeNull();
+      expect(queryByText($offline)).toBeNull();
+      expect(queryByText($slow)).toBeNull();
+      expect(queryByText($timeout)).toBeNull();
+      expect(queryByText($error)).toBeNull();
     });
   });
 
@@ -19,27 +24,25 @@ describe('NetworkStatus', () => {
     it('should show offline network notification', () => {
       const { queryByText } = render(<NetworkStatus isOffline />);
 
-      expect(queryByText(/Você está offline/i)).toBeInTheDocument();
+      expect(queryByText($offline)).toBeInTheDocument();
     });
 
     it('should show slow network notification', () => {
       const { queryByText } = render(<NetworkStatus isSlow />);
 
-      expect(queryByText(/Sua conexão parece está lenta/i)).toBeInTheDocument();
+      expect(queryByText($slow)).toBeInTheDocument();
     });
 
     it('should show request timeout notification', () => {
       const { queryByText } = render(<NetworkStatus hasTimeout />);
 
-      expect(
-        queryByText(/A API não conseguiu responder a tempo/i)
-      ).toBeInTheDocument();
+      expect(queryByText($timeout)).toBeInTheDocument();
     });
 
     it('should show error notification on response', () => {
       const { queryByText } = render(<NetworkStatus isError />);
 
-      expect(queryByText(/Houve um erro na simulação/i)).toBeInTheDocument();
+      expect(queryByText($error)).toBeInTheDocument();
     });
   });
 });
