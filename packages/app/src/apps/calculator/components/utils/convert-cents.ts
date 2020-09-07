b445962adfa9d3ad2b36a01d4662177ccd2740b3
Ozeas Santos <computer.ozeas@gmail.com>
2020-09-07 18:33:41 -0300

    feat: add test to result details

--- packages/app/src/apps/calculator/components/utils/convert-cents.ts
+++ packages/app/src/apps/calculator/components/utils/convert-cents.ts
@@ -1,7 +1,7 @@
 export const realToCents = (value: number): number => value * 100;
 export const centsToReal = (value: number): string => {
   const formattedValue = value / 100;
-  return new Intl.NumberFormat([], {
+  return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL'
   }).format(Number(formattedValue));
