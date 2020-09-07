20b392e308c517f425b927a2cb1660ca396e9d7a
Ozeas Santos <computer.ozeas@gmail.com>
2020-09-06 23:13:56 -0300

    fix: fix lint

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -4,8 +4,6 @@ import { Theme } from '@ac/components';
 
 import CalculatorPage from './apps/calculator/pages/calculator-page';
 
-console.log(Theme);
-
 const App: React.FC = () => (
   <Theme>
     <CalculatorPage />
