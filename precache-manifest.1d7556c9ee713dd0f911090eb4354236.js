521f2fb3e96d05ebed09e609d16d3500d8427f23
Ozeas Santos <computer.ozeas@gmail.com>
2020-09-07 23:34:24 -0300

    Updates

---
+++ precache-manifest.1d7556c9ee713dd0f911090eb4354236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc7ec7a99877bed50b64b7523581f86",
+    "url": "/app-calculator/index.html"
+  },
+  {
+    "revision": "106aedff744cffde4e3c",
+    "url": "/app-calculator/static/css/main.7f36fcc1.chunk.css"
+  },
+  {
+    "revision": "2127f09e73d869f37074",
+    "url": "/app-calculator/static/js/2.ea49addc.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/app-calculator/static/js/2.ea49addc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "106aedff744cffde4e3c",
+    "url": "/app-calculator/static/js/main.15cb8eeb.chunk.js"
+  },
+  {
+    "revision": "d4b9583852996254d9a5b908c50798f8",
+    "url": "/app-calculator/static/js/main.15cb8eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadd51093b116dfd3c8f",
+    "url": "/app-calculator/static/js/runtime-main.b157b9df.js"
+  }
+]);
