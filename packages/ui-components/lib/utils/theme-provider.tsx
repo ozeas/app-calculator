8d9fa611ba28140d3d49b66f30461b49369fc8fe
Ozeas Santos <computer.ozeas@gmail.com>
2020-09-08 10:03:43 -0300

    feat: add readme.md to project

--- packages/ui-components/lib/utils/theme-provider.tsx
+++ packages/ui-components/lib/utils/theme-provider.tsx
@@ -7,7 +7,7 @@ type Props = {
   children: ReactNode;
 };
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const Theme = ({ children }: Props) => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
